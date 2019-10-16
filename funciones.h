1443b7a9137130f3ee6fb7dbbe477e0c11a59edd
arlemgabriel <jogabra16@gmail.com>
2019-10-15 21:33:16 -0600

    Modificacion para hacer exit en caso de errores lexicos

---
+++ funciones.h
@@ -0,0 +1,139 @@
+#ifndef FUNCIONES_H_INCLUDED
+#define FUNCIONES_H_INCLUDED
+
+#include <vector>
+#include <iostream>
+#include "stdio.h"
+#include "stdlib.h"
+
+using namespace std;
+std::vector<int> row;
+std::vector<int> column;
+std::vector<string> token;
+std::vector<string> value;
+std::vector<string> errors;
+std::vector<int> columnerrors;
+std::vector<int> rowerrors;
+int errorssize;
+
+void addRow(int prow,int pcolumn,string ptoken,string pvalue)
+{
+    row.push_back(prow);
+    column.push_back(pcolumn);
+    token.push_back(ptoken);
+    value.push_back(pvalue);
+}
+
+void printErrors()
+{
+    for(int i=0;i<errorssize;i++)
+    {
+        std::cout << "ERROR: " << errors[i] << " on line:" << rowerrors[i] << " column:" << columnerrors[i]<<" \n";
+    }
+}
+
+void printTable()
+{
+    int rowssize = row.size();
+    int columnssize = column.size();
+    int tokenssize = token.size();
+    int valuessize = value.size();
+    errorssize = errors.size();
+    if(rowssize==columnssize && columnssize==tokenssize && tokenssize==valuessize){
+        if(rowssize!=0){
+          if(errorssize==0){
+              printf("%-8s%-8s%-16s%-8s\n", "LINE", "COLUMN", "TOKEN", "VALUE");
+              for(int i=0;i<row.size();i++)
+              {
+                  printf("%-8d%-8d%-16s%-8s\n",row[i],column[i],token[i].c_str(),value[i].c_str());
+              }
+          }else{
+            printErrors();
+            exit(0);
+          }
+
+        }
+    }else{
+        std::cout << "ERROR: It doesnt exist the same size of rows and columns";
+    }
+
+}
+void addError(int pRow,int pColumn,string pError){
+    errors.push_back(pError);
+    rowerrors.push_back(pRow);
+    columnerrors.push_back(pColumn);
+}
+
+
+/*
+class NodeParseTree
+{
+public:
+	NodeParseTree()
+	{
+<<<<<<< HEAD
+		hizq = NULL;
+=======
+		parent = NULL;
+>>>>>>> a52775577adac5b45cbe1135e9ccfd8bf1b675ad
+
+		token = "NULL";
+        value = "NULL";
+        row = -1;
+        column = -1;
+	}
+
+	NodeParseTree(string pToken, string pValue, int pRow, int pColumn)
+	{
+<<<<<<< HEAD
+		hizq = NULL;
+=======
+		parent = NULL;
+>>>>>>> a52775577adac5b45cbe1135e9ccfd8bf1b675ad
+
+		token = pToken;
+		value = pValue;
+		row = pRow;
+		column = pColumn;
+	}
+
+	string token;
+    string value;
+
+    int row;
+    int column;
+    NodeParseTree *parent;
+	vector<NodeParseTree> *childs;
+
+};
+
+typedef NodeParseTree * pNodeParseTree;
+
+class ParseTree
+{
+public:
+	ParseTree()
+	{
+		rootTree = actual = NULL;   //Constuctor
+	}
+	~ParseTree();                          //Deestructor
+	bool isVoid()
+	{
+		return rootTree == NULL;
+	}
+
+<<<<<<< HEAD
+	void newNode(string pCedula, string pNombre, string pDireccion, string pTel);
+=======
+	void newNode(string pToken, string pValue, int pRow, int pColumn);
+>>>>>>> a52775577adac5b45cbe1135e9ccfd8bf1b675ad
+	pNodeParseTree rootTree;
+
+
+private:
+	pNodeParseTree actual;
+};
+*/
+
+
+#endif // FUNCIONES_H_INCLUDED
